12ab0c8d0cddee04c1606acdfceedc8932fdf828
virrgoo <130205598+allsyariff@users.noreply.github.com>
2024-11-28 17:17:19 +0700

    Delete routes directory

--- routes/plantRoutes.js
+++
@@ -1,8 +0,0 @@
-/* const express = require('express');
-const { analyzeImage } = require('../controllers/plantController');
-const router = express.Router();
-
-router.post('/analyze', analyzeImage);
-
-module.exports = router;
-   */
