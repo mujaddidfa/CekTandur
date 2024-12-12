28f0fdc223f5cdd768009ab8ba417c9b4a149678
Mujaddid Fathi Athoillah <mujaddid201@gmail.com>
2024-12-12 13:14:22 +0700

    implement history detail page

--- MD/app/src/main/java/com/dicoding/cektandur/ui/HistoryAdapter.kt
+++ MD/app/src/main/java/com/dicoding/cektandur/ui/HistoryAdapter.kt
@@ -1,17 +1,38 @@
 package com.dicoding.cektandur.ui
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.cektandur.data.api.response.DataItem
 import com.dicoding.cektandur.databinding.HistoryPlantBinding
+import com.dicoding.cektandur.ui.history.HistoryDetailActivity
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class HistoryAdapter(private val historyList: List<DataItem>) : RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
 
     class HistoryViewHolder(private val binding: HistoryPlantBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(history: DataItem) {
-            binding.namaPenyakit.text = history.diseaseName
-            binding.date.text = history.timestamp
+            binding.tvPlantDisease.text = history.diseaseName
+
+            val originalFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+            val targetFormat = SimpleDateFormat("dd MMMM yyyy, HH:mm", Locale("id", "ID"))
+            val date = history.timestamp?.let { originalFormat.parse(it) }
+            val formattedDate = date?.let { targetFormat.format(it) }
+
+            binding.tvDate.text = formattedDate
+
+            binding.root.setOnClickListener {
+                val context = binding.root.context
+                val intent = Intent(context, HistoryDetailActivity::class.java).apply {
+                    putExtra("diseaseName", history.diseaseName)
+                    putExtra("timestamp", formattedDate)
+                    putExtra("confidence", history.confidence)
+                    putExtra("analysisResult", history.analysisResult)
+                }
+                context.startActivity(intent)
+            }
         }
     }
 
--- MD/app/src/main/java/com/dicoding/cektandur/ui/history/HistoryDetailActivity.kt
+++ MD/app/src/main/java/com/dicoding/cektandur/ui/history/HistoryDetailActivity.kt
@@ -1,16 +1,26 @@
 package com.dicoding.cektandur.ui.history
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.dicoding.cektandur.R
+import com.dicoding.cektandur.databinding.ActivityHistoryDetailBinding
 
 class HistoryDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHistoryDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history_detail)
-        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        binding = ActivityHistoryDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val diseaseName = intent.getStringExtra("diseaseName")
+        val timestamp = intent.getStringExtra("timestamp")
+        val confidence = intent.getStringExtra("confidence")
+        val analysisResult = intent.getStringExtra("analysisResult")
+        val confidenceInPercent = (confidence?.toFloat()?.times(100))?.toInt()
+
+        binding.tvPlantDisease.text = getString(R.string.plant_disease, diseaseName, confidenceInPercent)
+        binding.tvDescription.text = analysisResult
+        binding.tvTimestap.text = getString(R.string.analysis_date, timestamp)
     }
 }
